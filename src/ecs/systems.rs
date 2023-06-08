9df03f39ef6053f7de2868219aaacef81533796d
Aaron3154518 <aaoren210@gmail.com>
2023-06-07 21:39:37 -0600

    Fixed components macro to work recursively when called from any location

--- src/ecs/systems.rs
+++ src/ecs/systems.rs
@@ -1,1 +1,71 @@
+#[macro_export]
+macro_rules! components {
+        // Op => e
+        (@op) => {};
 
+        // Op => && NoOp
+        (@op && $($tail: tt)*) => {
+            $crate::components!(@no_op $($tail)*);
+        };
+
+        // Op => || NoOp
+        (@op || $($tail: tt)*) => {
+            $crate::components!(@no_op $($tail)*);
+        };
+
+        // NoOp => (NoOp) Op
+        (@no_op ($($inner: tt)*) $($tail: tt)*) => {
+            $crate::components!(@no_op $($inner)*);
+            $crate::components!(@op $($tail)*);
+        };
+
+        // Ty => :: ident Ty
+        (@ty ($($ty: ident),+) :: $i: ident $($tail: tt)*) => {
+            $crate::components!(@ty ($($ty),*,$i) $($tail)*);
+        };
+
+        // Ty => Op
+        (@ty ($($ty: ident),+) $($tail: tt)*) => {
+            const _: std::marker::PhantomData<$($ty)::*> = std::marker::PhantomData;
+            $crate::components!(@op $($tail)*);
+        };
+
+        // NoOp => ident Ty Op
+        (@no_op $i: ident $($tail: tt)*) => {
+            $crate::components!(@ty ($i) $($tail)*);
+        };
+
+        // NoOp => ! NoOp
+        (@no_op ! $($tail: tt)*) => {
+            $crate::components!(@no_op $($tail)*);
+        };
+
+        // S => e
+        (@labels) => {};
+
+        // S => ()
+        (@labels ()) => {};
+
+        // S => NoOp
+        (@labels $($tts: tt)*) => {
+            $crate::components!(@no_op $($tts)*);
+        };
+
+        (labels $labels: tt, $name: ident, $($n: ident: $t: ty),+) => {
+            $crate::components!(@labels $labels);
+            $crate::components!($name, $($n: $t),*);
+        };
+
+        ($name: ident, $($n: ident: $t: ty),+) => {
+            pub struct $name<'a> {
+                pub eid: &'a crate::_engine::Entity,
+                $(pub $n: $t),*
+            }
+
+            impl<'a> $name<'a> {
+                pub fn new(eid: &'a crate::_engine::Entity, $($n: $t),*) -> Self {
+                    Self { eid, $($n),* }
+                }
+            }
+        };
+    }
--- src/lib.rs
+++ src/lib.rs
@@ -22,76 +22,9 @@ game_crate!();
 pub mod test {
     use crate::{
         _engine::Entity,
+        // components,
         ecs::{components::Globals, entities::NewEntity, events::Event},
     };
-
-    macro_rules! components {
-        // Op => e
-        (@op) => {};
-
-        // Op => && NoOp
-        (@op && $($tail: tt)*) => {
-            components!(@no_op $($tail)*);
-        };
-
-        // Op => || NoOp
-        (@op || $($tail: tt)*) => {
-            components!(@no_op $($tail)*);
-        };
-
-        // NoOp => (NoOp) Op
-        (@no_op ($($inner: tt)*) $($tail: tt)*) => {
-            components!(@no_op $($inner)*);
-            components!(@op $($tail)*);
-        };
-
-        // Ty => :: ident Ty
-        (@ty ($($ty: ident),+) :: $i: ident $($tail: tt)*) => {
-            components!(@ty ($($ty),*,$i) $($tail)*);
-        };
-
-        // Ty => Op
-        (@ty ($($ty: ident),+) $($tail: tt)*) => {
-            const _: std::marker::PhantomData<$($ty)::*> = std::marker::PhantomData;
-            components!(@op $($tail)*);
-        };
-
-        // NoOp => ident Ty Op
-        (@no_op $i: ident $($tail: tt)*) => {
-            components!(@ty ($i) $($tail)*);
-        };
-
-        // NoOp => ! NoOp
-        (@no_op ! $($tail: tt)*) => {
-            components!(@no_op $($tail)*);
-        };
-
-        // S => e
-        (@labels) => {};
-
-        // S => NoOp
-        (@labels $($tts: tt)*) => {
-            components!(@no_op $($tts)*);
-        };
-
-        (labels $labels: tt, $name: ident, $($n: ident: $t: ty),+) => {
-            components!(@labels $labels);
-            components!($name, $($n: $t),*);
-        };
-
-        ($name: ident, $($n: ident: $t: ty),+) => {
-            pub struct $name<'a> {
-                pub eid: &'a crate::_engine::Entity,
-                $(pub $n: $t),*
-            }
-
-            impl<'a> $name<'a> {
-                pub fn new(eid: &'a crate::_engine::Entity, $($n: $t),*) -> Self {
-                    Self { eid, $($n),* }
-                }
-            }
-        };
-    }
 
     // Labels tests
     pub mod A {
@@ -100,17 +33,18 @@ pub mod test {
         }
     }
 
-    components!(@labels);
-    components!(@labels (!u8));
-    components!(@labels u8);
-    components!(@labels (u8));
-    components!(@labels u8 && i8);
-    components!(@labels (u8 || i8) && !!!u8);
-    components!(@labels (!(u8 || i8) && u8));
-    components!(@labels A::B::C);
-    components!(@labels String || !A::B::C);
+    crate::components!(@labels);
+    crate::components!(@labels (!u8));
+    crate::components!(@labels);
+    crate::components!(@labels u8);
+    crate::components!(@labels (u8));
+    crate::components!(@labels u8 && i8);
+    crate::components!(@labels (u8 || i8) && !!!u8);
+    crate::components!(@labels (!(u8 || i8) && u8));
+    crate::components!(@labels A::B::C);
+    crate::components!(@labels String || !A::B::C);
 
-    components!(
+    crate::components!(
         labels((TFoo || !A::B::C) && !A::B::C),
         QuxComponents,
         t: &'a TFoo,
