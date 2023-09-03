fc091c7915a8eff7ff3d4089dfa88dacdee5337a
Aaron3154518 <aaoren210@gmail.com>
2023-09-03 16:50:07 -0400

    Added helper methods to normalize angle values

--- src/utils/math.rs
+++ src/utils/math.rs
@@ -1,6 +1,24 @@
+use std::f32::consts::TAU;
+
 #[macro_export]
 macro_rules! f32 {
     ($e: expr) => {
         $e as f32
     };
+}
+
+pub trait NormalizeAngle {
+    fn normalize_rad(self) -> Self;
+
+    fn normalize_deg(self) -> Self;
+}
+
+impl NormalizeAngle for f32 {
+    fn normalize_rad(self) -> Self {
+        ((self % TAU) + TAU) % TAU
+    }
+
+    fn normalize_deg(self) -> Self {
+        ((self % 360.0) + 360.0) % 360.0
+    }
 }
